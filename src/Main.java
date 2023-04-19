3433e2d55d9bd0cc2134d7b9a9e12f12a96ebb1e
Сергей <bondarevser.bondarev@yandex.ru>
2023-04-19 15:25:36 +0300

    2 task

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte a = 12;
+        short b = 14;
+        int c = 16;
+        long d = 18L;
+        float e = 12.65f;
+        double f = 1.12443256;
+        System.out.println("значение переменной (a) с типом byte равно " + a);
+        System.out.println("значение переменной (b) с типом short равно " + b);
+        System.out.println("значение переменной (c) с типом byte равно " + c);
+        System.out.println("значение переменной (d) с типом byte равно " + d);
+        System.out.println("значение переменной (e) с типом byte равно " + e);
+        System.out.println("значение переменной (f) с типом byte равно " + f);
+
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        float a = 27.12f;
+        long b = 987_678_965_549L;
+        double c = 2.786;
+        int d = 569;
+        int e = -159;
+        short f = 27897;
+        byte g = 67;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
